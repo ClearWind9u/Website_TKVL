8ab2e44c05a1818f92929584d3c4770f3f4daf40
Bui Thanh Tung <164112205+tung09hcm@users.noreply.github.com>
2025-05-06 23:41:20 +0700

    Update User.js _ unique email

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
     name: String,
     userName: String,
-    userEmail: String,
+    userEmail: {
+        type: String,
+        unique: true,
+        required: true
+    },
     password: String,
     dayofBirth: Date,
     address: String,
